aab3f66483968dfb41ed5c544414e6936bcafbcc
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-10-25 17:26:34 +0600

    CourseDeatils component made responsive

--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
+import "./CourseDetails.css"
 
 const CourseDetails = () => {
     const course = useLoaderData().details[0]
     return (
-        <div className='border w-50 mt-2 mx-auto rounded pb-3'>
+        <div className='details-container border mt-2 px-2 mx-auto rounded pb-3'>
             <div className='p-2'>
-                <div className='d-flex justify-content-around'>
+                <div className='d-lg-flex justify-content-around'>
                     <div>
-                        <img className='rounded' src={course.img} alt="" />
+                        <img className='img-fluid rounded' src={course.img} alt="" />
                     </div>
-                    <div>
+                    <div className='text-center'>
                         <h4 className='font-bold'>{course.name}</h4>
                         <button className='bg-gray-400 rounded py-1 px-5 font-bold text-center mt-5'>Download</button>
                     </div>
                 </div>
-                <p className='px-3 my-1'>{course.description}</p>
+                <p className='px-3 my-1'>{course.description} <Link to="/courses">Go Back</Link></p>
                 <div className='text-center mt-2'>
-                    <button className='btn btn-primary w-50'>
+                    <button className='btn btn-primary btn-primium'>
                         <Link to="/checkout" className='text-light text-decoration-none' >Get Premium Access</Link>
                     </button>
                 </div>
