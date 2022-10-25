a727adffd8d7590d6c17e9601c0a9f9128ef4037
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-10-25 16:09:12 +0600

    Login component restyled with bootstrap

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,35 +1,43 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import "./Login.css"
 
 const Login = () => {
     return (
-        <div className='md:flex justify-between md:w-1/2 md:border-2 rounded mx-auto py-2 px-4 mt-2'>
+        <div className='form-container d-md-flex mx-auto gap-5 border mt-2 p-2'>
             <div>
-                <form className='bg-gray-200 px-2 py-10 border-2 rounded text-center'>
-                <h3 className='text-center font-bold text-xl mb-6'>Login Please</h3>
-                    <div className='mb-6'>
-                        <input type="email" name="email" id="email" placeholder='Your Email' className='px-2' />
-                    </div>
-                    <div className='mb-6'>
-                        <input type="password" name="password" id="password" placeholder='Your Password' className='px-2' />
-                    </div>
-                    <p className='text-center mb-4'>
-                        <button className='bg-blue-800 text-center py-1   px-20 rounded text-white' type="submit">Login</button>
-                    </p>
+                <Form className='bg-light p-2'>
+                    <h3 className='text-center'>Login Please</h3>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
 
-                    <small>Don't have an account? <Link to="/register" className='underline'>Register</Link> </small>
-                </form>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Accept our terms and conditions." />
+                    </Form.Group>
+                    <div className='text-center'>
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+                    </div>
+                </Form>
             </div>
-            <div className='md:border-l-2 md:pl-10 md:mt-1 mt-5'>
+            <div className='mt-2 signin-buttons'>
                 <div>
-                    <button className='bg-gray-300 border-2 rounded-md py-1 px-2 font-bold mb-4'>Signin with Google</button>
+                    <Button variant="primary d-block">Signin with Google</Button>
                 </div>
                 <div>
-                    <button className='bg-gray-300 border-2 rounded-md py-1 px-2 font-bold mb-4'>Signin with Github</button>
+                    <Button variant="primary d-block">Signin with Github</Button>
                 </div>
-                <div>
-                    <button className='bg-gray-300 border-2 rounded-md py-1 px-2 font-bold mb-4'>Signin with Facebook</button>
-                </div>
+               <div>
+                    <Button variant="primary d-block">Signin with Facebook</Button>
+               </div>
             </div>
         </div>
     );
