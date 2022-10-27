ae687a2ac4844c62ea46e0b17213911c24d70fd0
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-10-27 19:06:39 +0600

    Bugs fixed

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -8,7 +8,16 @@ const Checkout = () => {
     const { user, currentCourseInfo } = useContext(AuthContext);
     return (
         <>
-        <h4 className='text-center py-2'>Thanks for Choosing Our Course</h4>
+            <div className='d-flex justify-content-center  align-items-center gap-5'>
+                <h4 className='text-center py-2'>Thanks for Choosing Our Course</h4>
+                <p className='mt-2'>
+                    <ReactToPdf targetRef={ref} filename="course-invoice.pdf" x={40} y={50} scale={0.8}>
+                        {({ toPdf }) => (
+                            <button className='border bg-primary py-1 px-2 rounded text-white' onClick={toPdf}>Download Invoice</button>
+                        )}
+                    </ReactToPdf>
+                </p>
+            </div>
             <div ref={ref} className='checkout d-flex justify-content-between p-md-4  mt-2 rounded p-2 mx-auto text-white' style={{ backgroundColor: "#ff00c8"}}>
             <div className='border-end px-2'>
                 <h6>Course info</h6>
@@ -20,13 +29,7 @@ const Checkout = () => {
                 <p className='my-0'>Name: {user?.displayName}</p>
                 <p>Email: {user?.email ? user?.email : "Restcricted"}</p>
             </div>
-                <p>
-                    <ReactToPdf targetRef={ref} filename="course-invoice.pdf" x={40} y={50} scale={0.8}>
-                        {({ toPdf }) => (
-                            <button className='border bg-primary py-1 px-2 rounded text-white' onClick={toPdf}>Download Invoice</button>
-                        )}
-                    </ReactToPdf>
-            </p>
+                
         </div>
         </>
     );
