c7b9870b27da6e82315dadc5c1d5e00f3bd8501a
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-12-10 16:51:29 +0600

    Banner bugs fixed

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -5,7 +5,8 @@ const Banner = () => {
     return (
         <div className='banner'>
             <div className='content'>
-                <h2>World Class Learning Environment</h2>
+                <h2 className='text-white'>World Class Learning Environment</h2>
+                <p className='text-white'>Welcome to our learning platform. We provide the best qualitiful course. Our student are world wide spreaded. Aproximately, one hundred to five hundreds student have placed different countries.</p>
             </div>
         </div>
     );
